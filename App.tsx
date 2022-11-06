741cb93f8cc80275647e0b14547cab1b5b09ea23
Enzo Vieira <enzovieira16@hotmail.com>
2022-11-06 22:51:57 +0000

    Initial mobile setup

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import { StatusBar } from "native-base"
+import { NativeBaseProvider } from "native-base"
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto"
+
+import { THEME } from "./src/styles/theme"
+
+import { Loading } from "./src/components/Loading"
+import { SignIn } from "./src/screens/SignIn"
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  })
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {!fontsLoaded ? <Loading /> : <SignIn />}
+    </NativeBaseProvider>
+  )
+}
